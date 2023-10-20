5ecd57fdbb42268930af0a02f0ed7392bb08bb2d
Aniket A Tarale <83703321+anni-257@users.noreply.github.com>
2023-10-20 04:43:43 +0000

    variables and dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accId=3012;
+let email="anni-257@gmail.com";
+var name="Aniket Tarale";
+city="Pune";
+
+// console.log([accId,name,city,email]);
+console.table([accId,name,city,email]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+let state; // undefined
+
+// Data Types
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => placeholder
+// symbol => uniqueness
+// object => 
+
+// suppose we want temperature value (number) from server and if server didn't works and return 0 that is also a kind of temperature so instead of returning ZERO return null
